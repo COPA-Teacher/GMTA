6ec44265ad96bbe89502349b10e9772f7a5850a4
COPA-Teacher <copa.ranaghat.govt.iti@gmail.com>
2025-07-12 14:42:04 +0530

    Reconfigure to include test folders

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,9 +4,34 @@ export default defineConfig({
     test: {
         globals: true,
         environment: 'node',
-        include: ['src/**/*.spec.ts'],
+
+        // Include both `.spec.ts` and `.test.ts` files
+        include: [
+            'src/**/*.spec.ts',
+            'src/**/*.test.ts',
+            'src/__tests__/**/*.ts',
+        ],
+
+        // Exclude common build/temp/output directories
+        exclude: [
+            '**/node_modules/**',
+            '**/dist/**',
+            '**/.{idea,git,cache,output,temp}/**',
+            '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*',
+        ],
+
         coverage: {
             reporter: ['text', 'html'],
+            reportsDirectory: './coverage',
+            exclude: [
+                'src/types/**',
+                'src/configs/**',
+                '**/*.d.ts',
+                '**/__tests__/**/helpers/**',
+            ],
         },
+
+        watch: false,
+        allowOnly: true, // warn if `.only` is committed
     },
 });
